e4ddc75f93a4e23e1a5fc8a1d890bd1749021d0b
BielowAlex <drohomyretskyi.oleksandr@gmail.com>
2024-12-16 22:19:01 +0100

    feature: add mui theme

---
+++ src/feature/Header/components/Logo.tsx
@@ -0,0 +1,32 @@
+import QrCodeScannerIcon from '@mui/icons-material/QrCodeScanner';
+import { Typography, useTheme } from '@mui/material';
+import Link from 'next/link';
+import React from 'react';
+
+const Logo: React.FC = () => {
+  const { palette } = useTheme();
+
+  return (
+    <Link
+      href={'/'}
+      style={{
+        textDecoration: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        gap: 8,
+        color: palette.primary.contrastText,
+      }}
+    >
+      <QrCodeScannerIcon />
+      <Typography
+        fontWeight={'bold'}
+        fontSize={'large'}
+        color={palette.primary.contrastText}
+      >
+        QReasy
+      </Typography>
+    </Link>
+  );
+};
+
+export { Logo };
